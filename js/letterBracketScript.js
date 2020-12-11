e536b6546df3ade212e2f06e94baf8bc3748fa67
verike1996 <74845157+verike1996@users.noreply.github.com>
2020-12-11 13:47:16 -0500

    working on tourney bracket

---
+++ js/letterBracketScript.js
@@ -0,0 +1,6 @@
+// create variables for each letter questionButton
+// run an script when a button is clicked that adds the winner to a winner array and the loser to a loser array.
+let winnerArray = [];
+let loserArray = [];
+let contestantArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
+let basicHTML = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
