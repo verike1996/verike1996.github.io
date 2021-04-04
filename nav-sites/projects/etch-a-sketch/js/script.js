c7b4b8043e913d572c999ca00d3157d92b2350c3
Billy King <billyjack30@gmail.com>
2021-04-04 13:22:14 -0400

    added a functino that populates a grid with some event listeners

--- nav-sites/projects/etch-a-sketch/js/script.js
+++ nav-sites/projects/etch-a-sketch/js/script.js
@@ -1,6 +1,7 @@
 function createDiv() {
     let newDiv = document.createElement('div');
     newDiv.classList = 'gridElement';
+    newDiv.addEventListener('mouseover', changeColor);
     canvas.appendChild(newDiv);
 }
 
@@ -10,5 +11,25 @@ function fillGrid(size) {
     }
 }
 
+function setGridSize(size) {
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
+function createNewGrid(size) {
+    setGridSize(size);
+    fillGrid(size);
+}
+
+function defaultGrid() {
+    createNewGrid(16);
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
 const canvas = document.getElementById('canvas');
 const resetButton = document.getElementById('resetButton');
+
+window.addEventListener('load', defaultGrid);
