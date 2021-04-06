a0a4b2c6282124f45652dbda54e65b60010247ab
Billy King <billyjack30@gmail.com>
2021-04-05 22:21:15 -0400

    added a function that lets you change the color

--- nav-sites/projects/etch-a-sketch/js/script.js
+++ nav-sites/projects/etch-a-sketch/js/script.js
@@ -27,7 +27,16 @@ function defaultGrid() {
 }
 
 function changeColor(e) {
-    e.target.style.backgroundColor = 'black';
+    if (choice == 'custom') {
+        e.target.style.backgroundColor = customColor;
+    } else if (choice == 'grey') {
+        e.target.style.backgroundColor = 'grey';
+    } else if (choice == 'white') {
+        e.target.style.backgroundColor = 'white';
+    } else {
+        e.target.style.backgroundColor = 'black';
+    }
+    
 }
 
 function clearGrid() {
@@ -36,13 +45,42 @@ function clearGrid() {
     }
 }
 
+function changeGridSize() {
+    let newSize;
+    newSize = prompt('Pick a new canvas size between 1-100!');
+    if (newSize < 1 || newSize > 100 || newSize == NaN) {
+        alert('Pick a new canvas size between 1-100');
+        changeGridSize();
+    } else {
+        clearGrid();
+        createNewGrid(newSize);
+    }
+}
+
+function whatColor() {
+   
+}
+
+function userColorSelection(event) {
+    customColor = event.target.value;
+    choice = 'custom';
+}
+
 const canvas = document.getElementById('canvas');
-const resetButton = document.getElementById('resetButton');
+const changeButton = document.getElementById('changeButton');
+
+const greyChoice = document.getElementById('greyChoice');
+const blackChoice = document.getElementById('blackChoice');
+const whiteChoice = document.getElementById('whiteChoice');
+const colorPicker = document.getElementById('colorPicker');
 
 let color = 'black';
+let customColor;
+let choice;
 
 window.addEventListener('load', defaultGrid);
+changeButton.addEventListener('click', changeGridSize);
+colorPicker.addEventListener('change', userColorSelection, false)
 
-// still need to add a function that clears the grid so you can change the size
 //need to add a function that lets you change the color you draw with
 // need to add a function that changes the size of the grid
